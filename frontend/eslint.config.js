a5d63f343b027ba6a7015435421fcaed9406ef99
sophieisnull <pzen4027@uni.sydney.edu.au>
2025-03-22 23:49:03 +1100

    frontend homepage & card component

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,7 +1,7 @@
 import js from '@eslint/js'
-import globals from 'globals'
 import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
+import globals from 'globals'
 
 export default [
   { ignores: ['dist'] },
@@ -21,6 +21,7 @@ export default [
       'react-refresh': reactRefresh,
     },
     rules: {
+      "react/prop-types": "off",
       ...js.configs.recommended.rules,
       ...reactHooks.configs.recommended.rules,
       'no-unused-vars': ['error', { varsIgnorePattern: '^[A-Z_]' }],
